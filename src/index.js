3dc4babb7c5c5a591e97bb28c6f346a155940ff1
Agnia <agnia2000@inbox.ru>
2020-03-21 13:55:08 +0300

    feat: add solution to task

--- src/index.js
+++ src/index.js
@@ -37,8 +37,31 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+var MORSE_PAIRS = {
+    '10':  '.',
+    '11':  '-',
+  };
+
 function decode(expr) {
-    // write your solution here
+    expr = expr.split('');
+    let iterationCount = expr.length-1;
+    let str = '';
+    for (let j = 0; j < iterationCount; j = j + 10) {
+        let currentTenSymbols = expr.slice(j, j + 10);
+        if (currentTenSymbols[0] == '*')
+            str+=' ';
+        else 
+        {
+            let dottedDasArray = [];
+            for (let i = 0; i <= currentTenSymbols.length; i = i + 2) {
+                    let current =MORSE_PAIRS[currentTenSymbols[i] + currentTenSymbols[i + 1]];
+                    if (current)
+                    dottedDasArray.push(current);
+            }
+            str+=MORSE_TABLE[dottedDasArray.join('')];
+        }
+    }
+    return str;
 }
 
 module.exports = {
